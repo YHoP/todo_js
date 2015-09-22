8df3cb26a8bec18ae7126024de162a0f2fae9369
aimee reiss & yvonne Peng <student@epicodus.com>
2015-09-21 17:03:31 -0700

    Bug when click on task to append to completed

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,17 @@ $(document).ready(function() {
   var inputtedTask = $("input#new-task").val();
   var tasks = { taskName: inputtedTask, completed: false};
 
-  $("ul#toDo").append("<li><span class='glyphicon glyphicon-star'></span> <span class='tasks'>" + tasks.taskName + "</span></li>");
 
-  $(".tasks").last().click(function() {
+  $("ul#toDo").append("<li><span class='tasks'>" + tasks.taskName + "</span></li>");
+
+  $(".tasks").click(function() {
     tasks.completed = true;
-
-    $("ul#completed").append("<li><span class='glyphicon glyphicon-ok'></span>  <span class='complete'>" + tasks.taskName + "</span></li>");
-
-    $(".class").remove();
+    $("ul#completed").append("<li><span class='complete'>" + $(this).taskName + "</span></li>");
+    $(this).remove();
 
   }); // end of tasks last() click
+
+
 
   $("input#new-task").val("");
   }); // end of new-tasks submit
